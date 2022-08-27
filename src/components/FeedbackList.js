87c69925bc5e95dc63b4086ef89b51d1a0e5d12e
Mishen Thakshana <102714819+mishenThakshana@users.noreply.github.com>
2022-08-27 23:26:03 +0530

    Bug fixed

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -6,7 +6,7 @@ import FeedbackContext from "../context/FeedbackContext";
 const FeedbackList = () => {
   const { feedback } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length == 0) return <p>No feedback yet</p>;
+  if (!feedback || feedback.length === 0) return <p>No feedback yet</p>;
 
   return (
     <div className="feedback-list">
